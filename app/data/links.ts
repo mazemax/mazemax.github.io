6b80ceab423457a5549f4d886c42f6580584a0a2
Max <saad.siddiqui@mindvalley.com>
2025-03-06 00:34:58 +0800

    Updated CV link

--- app/data/links.ts
+++ app/data/links.ts
@@ -1,4 +1,4 @@
-export const resumeLink = 'https://drive.google.com/file/d/13d3O7SWfDwRtejnPcUD8fuRkoo597puR/view?usp=sharing';
+export const resumeLink = 'https://drive.google.com/file/d/1Y_hBOs2SFFcs6QZ0AtcJLTxoU-15PrLy/view';
 export const whatsAppLink = 'https://api.whatsapp.com/send?phone=60102440732';
 export const linkedinLink = 'https://www.linkedin.com/in/maxsaad';
 export const githubLink = 'https://github.com/mazemax';
